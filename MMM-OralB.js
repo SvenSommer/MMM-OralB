57677feba7c69657950ca2153e299245c74a8dd0
Thomas Mirlacher <thomas@mirlacher.at>
2018-03-03 22:53:07 +0100

    first take on a gui.

--- MMM-OralB.js
+++ MMM-OralB.js
@@ -12,7 +12,13 @@ Module.register('MMM-OralB', {
 	},
 
 	init() {
-		this.dataNotification = {};
+		this.notificationData = {};
+		this.gfx = [
+			'd="M 112 58 A 50 50 0 0 0 62 8 L 62 18 A 40 40 0 0 1 102 58 Z"',
+			'd="M 62 112 A 50 50 0 0 0 112 62 L 102 62 A 40 40 0 0 1 62 102 Z"',
+			'd="M 8 62 A 50 50 0 0 0 58 112 L 58 102 A 40 40 0 0 1 18 62 Z"',
+			'd="M 58 8 A 50 50 0 0 0 8 58 L 18 58 A 40 40 0 0 1 58 18 Z"'
+		];
 	},
 
 	// FIXME: it seems we need this call, otherwise the helper cannot send data???
@@ -20,9 +26,8 @@ Module.register('MMM-OralB', {
 		this.sendSocketNotification("start", {});
 	},
 
-	// FIXME: build a great UI
 	getTemplate() {
-		if (this.dataNotification.id) {
+		if (this.notificationData.id) {
 			return "loading...";
 		} else {
 			return 'MMM-OralB.njk';
@@ -30,12 +35,23 @@ Module.register('MMM-OralB', {
 	},
 
 	getTemplateData() {
-		return this.dataNotification;
+		return this.notificationData;
 	},
 
 	socketNotificationReceived(notification, payload) {
 		if (notification === 'MMM-OralB-DISPLAY_DATA') {
-			this.dataNotification = payload;
+			this.notificationData = payload;
+			this.notificationData.gfx = '<svg><g stroke="darkgrey" fill="none">';
+
+			var i=0;
+			for (;i<4 && i<payload.sector;i++) {
+				this.notificationData.gfx += `<path fill="darkgrey" ${this.gfx[i]}/>`;
+			}
+			for (;i<4;i++) {
+				this.notificationData.gfx += `<path ${this.gfx[i]}/>`;
+			}
+			this.notificationData.gfx += '</g></svg>';
+
 			this.updateDom();
 		}
 	},
